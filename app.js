150f5c92718d5fd42bda6243e2952e545cf02148
Sean <sean.cowan1@gmail.com>
2020-02-19 19:13:40 -0500

    assignment complete

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const games = require('./playstore');
+const app = express();
+
+app.use(morgan('common'));
+app.use(cors());
+
+app.get('/playstore', (req, res) => {
+    const { genre, sort } = req.query;
+
+    if (!genre && !sort) {
+        return res.json(games);
+    }
+
+    let foundGames = games.filter(game => {
+        if (game.Genres.toLowerCase().includes(genre.toLowerCase())) {
+            return game;
+        }
+    });
+
+    if (sort === "app") {
+        foundGames
+          .sort((a, b) => {
+            return a.App.toLowerCase() > b.App.toLowerCase() ? 1 : a.App.toLowerCase() < b.App.toLowerCase() ? -1 : 0;
+        });
+      }
+
+    if (sort === "rating") {
+        foundGames
+          .sort((a, b) => {
+            return a.Rating > b.Rating ? -1 : a.Rating < b.Rating ? 1 : 0;
+        });
+    }
+
+    res.json(foundGames);
+})
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+  });
